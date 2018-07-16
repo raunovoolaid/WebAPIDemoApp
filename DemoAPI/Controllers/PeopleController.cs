96139d58dd26db8380909d053cf8acffa3311330
Rauno Voolaid <rauno.voolaid@windowslive.com>
2018-07-16 23:04:33 +0300

    First Commit

---
+++ DemoAPI/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using DemoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoAPI.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        // GET: api/People
+        List<Person> people = new List<Person>();
+
+        public PeopleController()
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey", Id = 1 });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", Id = 2 });
+            people.Add(new Person { FirstName = "Bilbo", LastName = "Baggins", Id = 3 });
+        }
+        public List<Person> Get()
+        {
+            return people;
+        }
+
+        // GET: api/People/5
+        public Person Get(int id)
+        {
+            return people.Where(x => x.Id == id).FirstOrDefault();            
+        }
+
+        // POST: api/People
+        public void Post(Person val)
+        {
+            people.Add(val);
+        }
+                
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+            people.Remove(people.Where(x => x.Id == id).FirstOrDefault());
+        }
+    }
+    
+}
